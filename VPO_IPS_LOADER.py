db07e950da63e5bea4d2a483e92d26924c00bec3
Volodymyr Vasylenko <v.vasylenko92@gmail.com>
2022-04-29 06:59:40 +0300

    Add files via upload

--- VPO_IPS_LOADER.py
+++ VPO_IPS_LOADER.py
@@ -1,10 +1,9 @@
 import asyncio
-from genericpath import exists
-import os
 import aiohttp
 import datetime
 import json
-
+import pathlib
+import os
 
 async def ws_req():
     async with aiohttp.ClientSession() as session:
@@ -50,6 +49,26 @@ def gen_urls(cryptosrvip, daysArr, district):
     return urlsArr
 
 
+async def get_dov(url, session):
+    #print(url)
+    #cookies = {'PHPSESSID': phpsessid}
+    #conn = aiohttp.TCPConnector(limit=1)
+    
+    #while True:
+    #    try:
+    #async with aiohttp.ClientSession(cookies=cookies) as session:
+    
+        async with session.get(url) as response:
+        
+                #print(await response.json(content_type=None))
+            with open(r".\jsons\dovydka_{0}.json".format(url.split('/')[6]), "wb") as out:
+                async for chunk in response.content.iter_chunked(4096):
+                    out.write(chunk)
+        
+    #    except:
+    #        await asyncio.sleep(1)
+
+
 async def main(cryptosrvip, username, userpassword, urlsArr):
     PHPSESSID = await get_auth(cryptosrvip, username, userpassword)
     print(PHPSESSID)
@@ -57,9 +76,22 @@ async def main(cryptosrvip, username, userpassword, urlsArr):
     for url in urlsArr:
         task = asyncio.create_task(get_ips(url, PHPSESSID))
         tasks.add(task)
-    return await asyncio.gather(*tasks)
+    await asyncio.gather(*tasks)
     #print(tasks)
-
+    
+async def main2(CRYPTOSRVIP, USERNAME, USERPASSWORD, DovsUrl):
+    PHPSESSID = await get_auth(CRYPTOSRVIP, USERNAME, USERPASSWORD)
+    
+        
+    cookies = {'PHPSESSID': PHPSESSID}
+    
+    async with aiohttp.ClientSession(cookies=cookies) as session:
+        tasks = set()
+        for url in DovsUrl:
+            task = asyncio.create_task(get_dov(url, session))
+            tasks.add(task)
+        await asyncio.gather(*tasks)
+    
 
 if __name__ == '__main__':
     CRYPTOSRVIP = '127.0.0.1'
@@ -70,10 +102,27 @@ if __name__ == '__main__':
     if not os.path.exists(r'.\jsons'):
         os.makedirs('.\jsons')
     #asyncio.run(main(CRYPTOSRVIP, USERNAME, USERPASSWORD, gen_urls(CRYPTOSRVIP, gen_date_range(FROMDATE, TILLDATE), '1619')))
-    jsonsArr = []
+    DovsUrl = []
     for json_f in os.listdir(r'.\jsons'):
         if json_f[:3] == 'IPS':
             with open(r'.\jsons\{0}'.format(json_f)) as json_file:
-                data = json.loads(json_file.read())
-                print(data[0])
-    #print(print(jsonsArr[0].keys()))
+                data = json.load(json_file)
+                if len(data) != 0:
+                    for elem in data:
+                        #print(elem['id'])
+                        dov_numb = elem['id']
+                        if f'http://{CRYPTOSRVIP}:96/idp/rest/getview/{dov_numb}' not in DovsUrl:
+                            #print(f'http://{CRYPTOSRVIP}:96/idp/rest/getview/{dov_numb}'.split('/'))
+                            if pathlib.Path(r".\jsons\dovydka_{0}.json".format(f'http://{CRYPTOSRVIP}:96/idp/rest/getview/{dov_numb}'.split('/')[6])).exists():
+                                print(r".\jsons\dovydka_{0}.json".format(f'http://{CRYPTOSRVIP}:96/idp/rest/getview/{dov_numb}'.split('/')[6]))
+                                
+                            else:
+                                DovsUrl.append(f'http://{CRYPTOSRVIP}:96/idp/rest/getview/{dov_numb}')
+    print(len(DovsUrl))
+    asyncio.run(main2(CRYPTOSRVIP, USERNAME, USERPASSWORD, DovsUrl))
+    
+    
+    '''
+        raise asyncio.TimeoutError from None
+asyncio.exceptions.TimeoutError
+    '''
